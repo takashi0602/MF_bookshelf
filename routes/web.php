61ef595817bf933bf981d8169d55de44db739e54
Kazuki Tobita <kazukiti201@gmail.com>
2017-09-18 00:23:33 +0900

    ルートの定義

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,25 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Book;
+use Illuminate\Http\Request;
 
+/**
+ * 本のダッシュボード表示
+ */
 Route::get('/', function () {
-    return view('welcome');
+    return view('books');
+});
+
+/**
+ * 新「本」を追加
+ */
+Route::post('/books', function (Request $request) {
+    //
+});
+
+/**
+ * 本を削除
+ */
+Route::delete('/book/{book}', function (Book $book) {
+    //
 });
