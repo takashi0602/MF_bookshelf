68f87efabd4ebd865f76508fc77f81126518222f
Kazuki Tobita <kazukiti201@gmail.com>
2017-09-21 00:09:12 +0900

    バリデーションルールの追加
    Eloquentモデルの記述

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,24 @@ Route::get('/', function () {
  * 新「本」を追加
  */
 Route::post('/books', function (Request $request) {
-    //
+    // Varidation
+    $validator = Validator::make($request->all(), [
+        'item_name' => 'required | max: 255',
+    ]);
+
+    // Varidation Error
+    if ($validator->fails()) {
+        return redirect('/')->withInput()->withErrors($validator);
+    }
+
+    // Eloquent Model
+    $books = new Book;
+    $books->item_name = $request->item_name;
+    $books->item_number = '1';
+    $books->item_amount = '1000';
+    $books->published = '2017-03-07 00:00:00';
+    $books->save();
+    return redirect('/');
 });
 
 /**
