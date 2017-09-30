634a02861242b879b9debc3da06e02d3ec02d2de
Kazuki Tobita <kazukiti201@gmail.com>
2017-10-01 03:46:55 +0900

    マイグレーションの修正

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,8 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->nullable();
             $table->string('password', 60)->nullable();
-            $table->string('provider');
-            $table->string('provider_id');
+            $table->string('provider')->default('bookshelf');
+            $table->string('provider_id')->default('bookshelf');
             $table->rememberToken();
             $table->timestamps();
         });
