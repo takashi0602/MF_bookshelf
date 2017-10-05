5667edacaca79de911ad3e38687b56ef91ddbfce
Kazuki Tobita <kazukiti201@gmail.com>
2017-10-05 23:25:50 +0900

    Social Buttons for Bootstrapの導入

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,13 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Styles -->
-
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="{{ asset('css/assets/css/bootstrap.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/assets/css/font-awesome.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/assets/css/docs.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/bootstrap-social.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
         <div id="app">
