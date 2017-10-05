9d38478cba0f8ebc48ea481f82bbe6f8977c96b8
Kazuki Tobita <kazukiti201@gmail.com>
2017-10-06 00:28:31 +0900

    シーダーの作成

---
+++ database/seeds/BooksTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create('ja_JP');
+        for ($i = 0; $i < 10; $i++) {
+            App\Book::create([
+                'user_id' => $faker->numberBetween(1, 3),
+                'item_name' => $faker->word(),
+                'item_number' => $faker->numberBetween(1, 999),
+                'item_amount' => $faker->numberBetween(100, 5000),
+                'published' => $faker->dateTime('now'),
+                'created_at' => $faker->dateTime('now'),
+                'updated_at' => $faker->dateTime('now'),
+            ]);
+        }
+    }
+}
