14c43da27e89522ab0cf0d80f0b396377898cd46
Kazuki Tobita <kazukiti201@gmail.com>
2017-10-24 13:41:52 +0900

    ヘッダー文言修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,8 +49,8 @@
                         <ul class="nav navbar-nav navbar-right">
                             <!-- Authentication Links -->
                             @guest
-                            <li><a href="{{ route('login') }}">ログイン</a></li>
-                            <li><a href="{{ route('register') }}">登録</a></li>
+                            <li><a href="{{ route('login') }}">サインイン</a></li>
+                            <li><a href="{{ route('register') }}">サインアップ</a></li>
                             @else
                                 <li class="dropdown">
                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
