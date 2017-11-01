d00e1a71c87cbd5f54280800b1807b2f45e7578f
Kazuki Tobita <kazukiti201@gmail.com>
2017-11-01 22:04:20 +0900

    トップページのマークアップ修正
    fixed #152

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,30 +7,26 @@
             <div class="c-catchphrase">知識の海を泳げ。</div>
             @if (count($books) > 0)
                 <div class="c-publicBooks_title">みんなの本棚</div>
-                @foreach ($books as $book)
-                    @if ($book->flag === 'public')
-                        <ul class="c-publicBooks_field">
-                            <li class="c-publicBooks_img">
-                                <div><img src="data:image/png;base64,{{ $book->book_img }}" alt="" width="100"></div>
-                            </li>
-                            <li class="c-publicBooks_name">
-                                <div>{{ $book->book_name }}</div>
-                            </li>
-                            <li class="c-publicBooks_detail">
-                                <form action="{{ url('public/detail/' . $book->id) }}" method="POST">
-                                    {{ csrf_field() }}
-                                    <button type="submit">
-                                        <i class="fa fa-search" aria-hidden="true"></i> 詳細
-                                    </button>
-                                </form>
-                            </li>
-                        </ul>
-                    @endif
-                @endforeach
+                <div class="c-publicBooks_field">
+                    @foreach ($books as $book)
+                        @if ($book->flag === 'public')
+                            <ul>
+                                <li class="c-publicBooks_img"><img src="data:image/png;base64,{{ $book->book_img }}" alt="" width="100"></li>
+                                <li class="c-publicBooks_name">{{ $book->book_name }}</li>
+                                <li class="c-publicBooks_detail">
+                                    <form action="{{ url('public/detail/' . $book->id) }}" method="POST">
+                                        {{ csrf_field() }}
+                                        <button type="submit" class="c-publicBooks_btn">
+                                            <i class="fa fa-search" aria-hidden="true"></i> 詳細
+                                        </button>
+                                    </form>
+                                </li>
+                            </ul>
+                        @endif
+                    @endforeach
+                </div>
             @endif
-            <div class="c-publicBooks_link">
-                <a href="/public"><i class="fa fa-search" aria-hidden="true"></i>もっと見る</a>
-            </div>
+            <a href="/public" class="c-publicBooks_link"><i class="fa fa-search" aria-hidden="true"></i> もっと見る</a>
         </div>
     </div>
 @endsection
