75982c5f6685a239a6bb2233cb172bddc1cd8b11
takashi <b7180@oic.jp>
2017-11-02 00:08:58 +0900

    自分の本棚　マークアップ

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -1,113 +1,94 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        @include('common.errors')
-        <form enctype="multipart/form-data" action="{{ url('private/books') }}" method="POST">
-            {{ csrf_field() }}
-            <div>
-                <div>
-                    <label for="book-name">書籍名</label>
-                    <input type="text" name="book_name" id="book-name">
-                </div>
-                <div>
-                    <label for="book-price">価格</label>
-                    <input type="number" name="book_price" id="book-price">
-                </div>
-                <div>
-                    <label for="book-page">ページ数</label>
-                    <input type="number" name="book_page" id="book-page">
-                </div>
-                <div>
-                    <label for="published">出版日</label>
-                    <input type="date" name="published" id="published">
-                </div>
-                <div>
-                    <label for="author">著者</label>
-                    <input type="text" name="author" id="author">
-                </div>
-                <div>
-                    <label for="book-description">説明</label>
-                    <textarea name="book_description" id="book-description"></textarea>
-                </div>
-                <div>
-                    <label for="book-img">画像</label>
-                    <input type="file" name="book_img" id="book-img">
-                </div>
-                <div>
-                    <label for="flag">公開設定</label>
-                    <input type="radio" name="flag" id="flag" value="public" checked> 公開
-                    <input type="radio" name="flag" id="flag" value="private"> 非公開
-                </div>
+    <div id="p-myBooks">
+        <div class="c-container">
+            @include('common.errors')
+            <form enctype="multipart/form-data" action="{{ url('private/books') }}" method="POST">
+                {{ csrf_field() }}
+                <div class="c-createBook_title">本の追加</div>
+                <a href="#">ISBNで検索する</a>
+                <ul class="c-createBook">
+                    <li class="c-createBook_item">
+                        <label for="book-name">書籍名</label>
+                        <input type="text" name="book_name" id="book-name">
+                    </li>
+                    <li class="c-createBook_item">
+                        <label for="book-price">価格</label>
+                        <input type="number" name="book_price" id="book-price">
+                    </li>
+                    <li class="c-createBook_item">
+                        <label for="book-page">ページ数</label>
+                        <input type="number" name="book_page" id="book-page">
+                    </li>
+                    <li class="c-createBook_item">
+                        <label for="published">出版日</label>
+                        <input type="date" name="published" id="published">
+                    </li>
+                    <li class="c-createBook_item">
+                        <label for="author">著者</label>
+                        <input type="text" name="author" id="author">
+                    </li>
+                    <li class="c-createBook_item">
+                        <label for="book-description">説明</label>
+                        <textarea name="book_description" id="book-description"></textarea>
+                    </li>
+                    <li class="c-createBook_item">
+                        <label for="book-img">画像</label>
+                        <input type="file" name="book_img" id="book-img">
+                    </li>
+                    <li class="c-createBook_item">
+                        <label for="flag">公開設定</label>
+                        <input type="radio" name="flag" id="flag" value="public" checked> 公開
+                        <input type="radio" name="flag" id="flag" value="private"> 非公開
+                    </li>
+                </ul>
+                <button type="submit" class="c-createBook_btn">
+                    <i class="fa fa-plus" aria-hidden="true"></i> 本棚に入れる
+                </button>
+            </form>
+            @if (count($books) > 0)
+                <div class="c-myBooks">本一覧</div>
+                @foreach ($books as $book)
+                    <ul class="c-myBooksList">
+                        <li class="c-myBooksList_img">
+                            <div><img src="data:image/png;base64,{{ $book->book_img }}" alt="" width="100"></div>
+                        </li>
+                        <li class="c-myBooksList_name">
+                            <div>{{ $book->book_name }}</div>
+                        </li>
+                        <li class="c-publicBooks_detail">
+                            <form action="{{ url('public/detail/' . $book->id) }}" method="POST">
+                                {{ csrf_field() }}
+                                <button type="submit" class="c-publicBooks_btn">
+                                    <i class="fa fa-search" aria-hidden="true"></i> 詳細
+                                </button>
+                            </form>
+                        </li>
+                    </ul>
+                @endforeach
+            @endif
+            <div class="c-paginate">
+                {{ $books->links()}}
             </div>
-            <div>
-                <div>
-                    <button type="submit">
-                        <i class="fa fa-plus" aria-hidden="true"></i> 本棚に入れる
-                    </button>
-                </div>
-            </div>
-        </form>
-        @if (count($books) > 0)
-            <div>
-                <div>本一覧</div>
-                <div>
-                    <table>
-                        <thead>
-                            <th>&nbsp;</th>
-                            <th>書籍名</th>
-                            <th>ページ数</th>
-                            <th>価格</th>
-                        </thead>
-                        <tbody>
-                            @foreach ($books as $book)
-                                <tr>
-                                    <td>
-                                        <div><img src="data:image/png;base64,{{ $book->book_img }}" alt="" width="100"></div>
-                                    </td>
-                                    <td>
-                                        <div>{{ $book->book_name }}</div>
-                                    </td>
-                                    <td>
-                                        @if($book->book_page === null)
-                                            <div>-</div>
-                                        @else
-                                            <div>{{ $book->book_page }} ページ</div>
-                                        @endif
-                                    </td>
-                                    <td>
-                                        @if($book->book_price === null)
-                                            <div>-</div>
-                                        @else
-                                            <div>￥ {{ $book->book_price }}</div>
-                                        @endif
-                                    </td>
-                                    <td>
-                                        <form action="{{ url('private/books/edit/' . $book->id) }}" method="POST">
-                                            {{ csrf_field() }}
-                                            <button type="submit">
-                                                <i class="fa fa-pencil" aria-hidden="true"></i> 更新
-                                            </button>
-                                        </form>
-                                    </td>
-                                    <td>
-                                        <form action="{{ url('private/book/' . $book->id) }}" method="POST">
-                                            {{ csrf_field() }}
-                                            {{ method_field('DELETE') }}
-                                            <button type="submit">
-                                                <i class="fa fa-trash" aria-hidden="true"></i> 削除
-                                            </button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        @endif
-        <div>
-            {{ $books->links()}}
         </div>
     </div>
 @endsection
+
+{{--<li>--}}
+{{--<form action="{{ url('private/books/edit/' . $book->id) }}" method="POST">--}}
+{{--{{ csrf_field() }}--}}
+{{--<button type="submit">--}}
+{{--<i class="fa fa-pencil" aria-hidden="true"></i> 更新--}}
+{{--</button>--}}
+{{--</form>--}}
+{{--</li>--}}
+{{--<li>--}}
+{{--<form action="{{ url('private/book/' . $book->id) }}" method="POST">--}}
+{{--{{ csrf_field() }}--}}
+{{--{{ method_field('DELETE') }}--}}
+{{--<button type="submit">--}}
+{{--<i class="fa fa-trash" aria-hidden="true"></i> 削除--}}
+{{--</button>--}}
+{{--</form>--}}
+{{--</li>--}}
