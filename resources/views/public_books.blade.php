bb688a3a6eec1782713965f6dcf7a0ff540f812d
Kazuki Tobita <kazukiti201@gmail.com>
2017-11-02 10:38:52 +0900

    公開範囲修正
    fixed #167

--- resources/views/public_books.blade.php
+++ resources/views/public_books.blade.php
@@ -17,38 +17,36 @@
                         </thead>
                         <tbody>
                             @foreach ($books as $book)
-                                @if ($book->flag === 'public' )
-                                    <tr>
-                                        <td>
-                                            <div><img src="data:image/png;base64,{{ $book->book_img }}" alt="" width="100"></div>
-                                        </td>
-                                        <td>
-                                            <div>{{ $book->book_name }}</div>
-                                        </td>
-                                        <td>
-                                            @if($book->book_page === null)
-                                                <div>-</div>
-                                            @else
-                                                <div>{{ $book->book_page }} ページ</div>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            @if($book->book_price === null)
-                                                <div>-</div>
-                                            @else
-                                                <div>￥ {{ $book->book_price }}</div>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            <form action="{{ url('public/detail/' . $book->id) }}" method="POST">
-                                                {{ csrf_field() }}
-                                                <button type="submit">
-                                                    <i class="fa fa-search" aria-hidden="true"></i> 詳細
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endif
+                                <tr>
+                                    <td>
+                                        <div><img src="data:image/png;base64,{{ $book->book_img }}" alt="" width="100"></div>
+                                    </td>
+                                    <td>
+                                        <div>{{ $book->book_name }}</div>
+                                    </td>
+                                    <td>
+                                        @if($book->book_page === null)
+                                            <div>-</div>
+                                        @else
+                                            <div>{{ $book->book_page }} ページ</div>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @if($book->book_price === null)
+                                            <div>-</div>
+                                        @else
+                                            <div>￥ {{ $book->book_price }}</div>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <form action="{{ url('public/detail/' . $book->id) }}" method="POST">
+                                            {{ csrf_field() }}
+                                            <button type="submit">
+                                                <i class="fa fa-search" aria-hidden="true"></i> 詳細
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
                             @endforeach
                         </tbody>
                     </table>
