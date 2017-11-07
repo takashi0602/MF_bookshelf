58a43bd5e6edda6b4b1b997e703af09cb943984d
Kazuki Tobita <kazukiti201@gmail.com>
2017-11-07 12:15:59 +0900

    common.errors ブレードファイル マークアップ修正
    fixed #220

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -1,11 +1,11 @@
 @if (count($errors) > 0)
     <div class="p-errors">
-        <div class="c-errorsText"><strong>入力した文字を修正してください。</strong></div>
-        <div class="c-errorsForm">
+        <strong>入力した文字を修正してください。</strong>
+        <div class="c-errors">
             <ul>
-            @foreach ($errors->all() as $error)
-                <li class="c-errorMsg">{{ $error }}</li>
-            @endforeach
+                @foreach ($errors->all() as $error)
+                    <li class="c-error">{{ $error }}</li>
+                @endforeach
             </ul>
         </div>
     </div>
