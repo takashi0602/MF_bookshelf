4ac53aaa55da37fed1a4b0003e558bbba25016a4
Kazuki Tobita <kazukiti201@gmail.com>
2017-11-08 10:57:10 +0900

    スタイル修正
    fixed #271
    
    fixed #270
    
    fixed #268

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,14 +11,14 @@
                     <h2>Bookshelfアカウントでサインイン</h2>
                     <ul class="c-lists">
                         <li class="form-group{{ $errors->has('email') ? ' has-error' : '' }} c-list">
-                            <label class="c-label" for="email">メールアドレス</label>
+                            <label class="c-label" for="email">メールアドレス <span class="c-required">*</span></label>
                             <input class="c-email" id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
                             @if ($errors->has('email'))
                                 <strong>{{ $errors->first('email') }}</strong>
                             @endif
                         </li>
                         <li class="form-group{{ $errors->has('password') ? ' has-error' : '' }} c-list">
-                            <label class="c-label" for="password">パスワード</label>
+                            <label class="c-label" for="password">パスワード <span class="c-required">*</span></label>
                             <input class="c-password" id="password" type="password" name="password" required>
                             @if ($errors->has('password'))
                                 <strong>{{ $errors->first('password') }}</strong>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,28 +11,28 @@
                     <h2>Bookshelfにサインアップ</h2>
                     <ul class="c-lists">
                         <li class="form-group{{ $errors->has('name') ? ' has-error' : '' }} c-list">
-                            <label class="c-label" for="name">ユーザネーム</label>
+                            <label class="c-label" for="name">ユーザネーム <span class="c-required">*</span></label>
                             <input class="c-textBox" id="name" type="text" name="name" value="{{ old('name') }}" required autofocus>
                             @if ($errors->has('name'))
                                 <strong>{{ $errors->first('name') }}</strong>
                             @endif
                         </li>
                         <li class="form-group{{ $errors->has('email') ? ' has-error' : '' }} c-list">
-                            <label class="c-label" for="email">メールアドレス</label>
+                            <label class="c-label" for="email">メールアドレス <span class="c-required">*</span></label>
                             <input class="c-email" id="email" type="email" name="email" value="{{ old('email') }}" required>
                             @if ($errors->has('email'))
                                 <strong>{{ $errors->first('email') }}</strong>
                             @endif
                         </li>
                         <li class="form-group{{ $errors->has('password') ? ' has-error' : '' }} c-list">
-                            <label class="c-label" for="password">パスワード</label>
+                            <label class="c-label" for="password">パスワード <span class="c-required">*</span></label>
                             <input class="c-password" id="password" type="password" name="password" required>
                             @if ($errors->has('password'))
                                 <strong>{{ $errors->first('password') }}</strong>
                             @endif
                         </li>
                         <li class="c-list">
-                            <label class="c-label" for="password-confirm">パスワード(再入力)</label>
+                            <label class="c-label" for="password-confirm">パスワード(再入力) <span class="c-required">*</span></label>
                             <input class="c-password" id="password-confirm" type="password" name="password_confirmation" required>
                         </li>
                     </ul>
