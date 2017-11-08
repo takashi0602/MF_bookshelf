75a805a6f2e75670660fb481f37278147eab064d
Kazuki Tobita <kazukiti201@gmail.com>
2017-11-08 11:01:59 +0900

    マイページ修正
    fixed #267

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <div class="p-mypage">
         <div class="c-container">
-            <h1 class="c-mypage_title">マイページ</h1>
-            <ul class="c-mypage_profiles">
-                <li class="c-mypage_profile">
+            <h1 class="c-title">マイページ</h1>
+            <ul class="c-lists">
+                <li class="c-list">
                     <div class="c-mypage_profileName">名前：{{ Auth::user()->name }}</div>
                 </li>
-                <li class="c-mypage_profile">
+                <li class="c-list">
                     <div class="c-mypage_profileBook">登録した本：{{ count($books) }}冊</div>
                 </li>
             </ul>
