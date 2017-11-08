bd61e3d4c3f4d3b687bbeffae678591ae4af5ca1
Kazuki Tobita <kazukiti201@gmail.com>
2017-11-09 01:31:25 +0900

    書籍編集画面 スタイル修正

--- resources/views/books_edit.blade.php
+++ resources/views/books_edit.blade.php
@@ -3,71 +3,73 @@
 @section('content')
     <div class="p-edit">
         <div class="c-container">
-            @include('common.errors')
-            <form action="{{ url('private/books/update') }}" method="POST">
+            <div class="c-contents">
                 <h1 class="c-title">本の編集</h1>
-                <ul class="c-lists">
-                    <li class="c-list">
-                        @if(preg_match("/^.\/img\/default_books\/book_/", $book->book_img))
-                            <img src="{{ substr($book->book_img, 1) }}" alt="" width="300">
-                        @elseif(preg_match("/^http:\/\//", $book->book_img))
-                            <img src="{{ $book->book_img }}" alt="" width="300">
-                        @else
-                            <img src="data:image/png;base64,{{ $book->book_img }}" alt="" width="300">
-                        @endif
-                    </li>
-                    <li class="c-list">
-                        <label class="c-label" for="book-name">書籍名 <span class="c-required">*</span></label>
-                        <input class="c-textBox" type="text" name="book_name" id="book-name" value="{{ $book->book_name }}">
-                    </li>
-                    <li class="c-list">
-                        <label class="c-label" for="book-page">ページ数</label>
-                        <input class="c-number" type="number" name="book_page" id="book-page" value="{{ $book->book_page }}">
-                    </li>
-                    <li class="c-list">
-                        <label class="c-label" for="published">出版日</label>
-                        <input class="c-date" type="date" name="published" id="published" value="{{ $book->published }}">
-                    </li>
-                    <li class="c-list">
-                        <label class="c-label" for="author">著者</label>
-                        <input class="c-textBox" type="text" name="author" id="author" value="{{ $book->author }}">
-                    </li>
-                    <li class="c-list">
-                        <label class="c-label" for="book-description">説明</label>
-                        <textarea name="book_description" id="book-description" class="c-textArea">{{ $book->book_description }}</textarea>
-                    </li>
-                    <li class="c-list">
-                        <label class="c-label" for="flag">公開設定</label>
-                        @if ($book->flag === 'public')
-                            <input class="c-radio" type="radio" name="flag" id="flag" value="public" checked> 公開
-                            <input class="c-radio" type="radio" name="flag" id="flag" value="private"> 非公開
-                        @else
-                            <input class="c-radio" type="radio" name="flag" id="flag" value="public"> 公開
-                            <input class="c-radio" type="radio" name="flag" id="flag" value="private" checked> 非公開
-                        @endif
-                    </li>
-                </ul>
-                <div class="c-editBook_Btn">
-                    <button class="c-btn_large" type="submit">
-                        <i class="fa fa-pencil" aria-hidden="true"></i> 本を更新
-                    </button>
-                    <input type="hidden" name="id" value="{{ $book->id }}">
-                    {{ csrf_field() }}
-                </div>
-            </form>
-            <div class="c-editBook_Btn">
-                <form action="{{ url('private/book/' . $book->id) }}" method="POST">
-                    {{ csrf_field() }}
-                    {{ method_field('DELETE') }}
-                    <button type="submit" class="c-btn_large">
-                        <i class="fa fa-trash" aria-hidden="true"></i> 本を捨てる
-                    </button>
+                <form action="{{ url('private/books/update') }}" method="POST">
+                    @include('common.errors')
+                    <ul class="c-lists">
+                        <li class="c-list">
+                            @if(preg_match("/^.\/img\/default_books\/book_/", $book->book_img))
+                                <img src="{{ substr($book->book_img, 1) }}" alt="" width="300">
+                            @elseif(preg_match("/^http:\/\//", $book->book_img))
+                                <img src="{{ $book->book_img }}" alt="" width="300">
+                            @else
+                                <img src="data:image/png;base64,{{ $book->book_img }}" alt="" width="300">
+                            @endif
+                        </li>
+                        <li class="c-list">
+                            <label class="c-label" for="book-name">書籍名 <span class="c-required">*</span></label>
+                            <input class="c-textBox" type="text" name="book_name" id="book-name" value="{{ $book->book_name }}">
+                        </li>
+                        <li class="c-list">
+                            <label class="c-label" for="book-page">ページ数</label>
+                            <input class="c-number" type="number" name="book_page" id="book-page" value="{{ $book->book_page }}">
+                        </li>
+                        <li class="c-list">
+                            <label class="c-label" for="published">出版日</label>
+                            <input class="c-date" type="date" name="published" id="published" value="{{ $book->published }}">
+                        </li>
+                        <li class="c-list">
+                            <label class="c-label" for="author">著者</label>
+                            <input class="c-textBox" type="text" name="author" id="author" value="{{ $book->author }}">
+                        </li>
+                        <li class="c-list">
+                            <label class="c-label" for="book-description">説明</label>
+                            <textarea name="book_description" id="book-description" class="c-textArea">{{ $book->book_description }}</textarea>
+                        </li>
+                        <li class="c-list">
+                            <label class="c-label" for="flag">公開設定</label>
+                            @if ($book->flag === 'public')
+                                <input class="c-radio" type="radio" name="flag" id="flag" value="public" checked> 公開
+                                <input class="c-radio" type="radio" name="flag" id="flag" value="private"> 非公開
+                            @else
+                                <input class="c-radio" type="radio" name="flag" id="flag" value="public"> 公開
+                                <input class="c-radio" type="radio" name="flag" id="flag" value="private" checked> 非公開
+                            @endif
+                        </li>
+                    </ul>
+                    <div class="c-editBook_Btn">
+                        <button class="c-btn_large" type="submit">
+                            <i class="fa fa-pencil" aria-hidden="true"></i> 本を更新
+                        </button>
+                        <input type="hidden" name="id" value="{{ $book->id }}">
+                        {{ csrf_field() }}
+                    </div>
                 </form>
-            </div>
-            <div class="c-backLink">
-                <a href="{{ url('/private') }}" class="c-link">
-                    <i class="fa fa-chevron-left" aria-hidden="true"></i> 戻る
-                </a>
+                <div class="c-editBook_Btn">
+                    <form action="{{ url('private/book/' . $book->id) }}" method="POST">
+                        {{ csrf_field() }}
+                        {{ method_field('DELETE') }}
+                        <button type="submit" class="c-btn_large">
+                            <i class="fa fa-trash" aria-hidden="true"></i> 本を捨てる
+                        </button>
+                    </form>
+                </div>
+                <div class="c-backLink">
+                    <a href="{{ url('/private') }}" class="c-link u-link">
+                        <i class="fa fa-chevron-left" aria-hidden="true"></i> 戻る
+                    </a>
+                </div>
             </div>
         </div>
     </div>
