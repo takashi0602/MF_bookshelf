5f48fca9a7ec675c8bfedf417ddf26e358e6bc5a
Kazuki Tobita <kazukiti201@gmail.com>
2017-11-11 02:04:23 +0900

    マイページ スタイル
    fixed #212

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -10,10 +10,10 @@
                 <h1 class="c-title">マイページ</h1>
                 <ul class="c-lists">
                     <li class="c-list">
-                        <div class="c-mypage_profileName">名前：{{ Auth::user()->name }}</div>
+                        <div class="c-mypage_status">名前：{{ Auth::user()->name }}</div>
                     </li>
                     <li class="c-list">
-                        <div class="c-mypage_profileBook">登録した本：{{ count($books) }}冊</div>
+                        <div class="c-mypage_status">登録した本：{{ count($books) }}冊</div>
                     </li>
                 </ul>
             </div>
