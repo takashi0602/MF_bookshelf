88b7ed6668436b8637e94787cbffb5d6385c514f
Kazuki Tobita <kazukiti201@gmail.com>
2017-11-11 01:23:57 +0900

    パスワードリセット画面 修正
    fixed #138
    
    fixed #116

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -5,9 +5,9 @@
 
 @section('nav')
 <div class="p-passwordReset">
-    <div class="container">
-        <h1>パスワードを再設定します</h1>
-        <div class="c-passwordReset_form">
+    <div class="c-container">
+        <div class="c-contents u-contents_email">
+            <h1 class="c-title">パスワード再設定</h1>
             @if (session('status'))
                 <div>
                     {{ session('status') }}
@@ -15,16 +15,16 @@
             @endif
             <form method="POST" action="{{ route('password.email') }}">
                 {{ csrf_field() }}
-                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }} c-passwordReset_email">
-                    <label class="c-passwordReset_label" for="email">メールアドレス</label>
-                    <input class="c-passwordReset_textBox" id="email" type="email" name="email" value="{{ old('email') }}" required>
-                    @if ($errors->has('email'))
-                        <strong>{{ $errors->first('email') }}</strong>
-                    @endif
-                </div>
-                <div class="c-passwordReset_btn">
-                    <button class="c-sendBtn" type="submit">メールを送信</button>
-                </div>
+                <ul class="c-lists">
+                    <li class="form-group{{ $errors->has('email') ? ' has-error' : '' }} c-list">
+                        <label class="c-label" for="email">メールアドレス</label>
+                        <input class="c-email" id="email" type="email" name="email" value="{{ old('email') }}" required autofocus>
+                        @if ($errors->has('email'))
+                            <strong>{{ $errors->first('email') }}</strong>
+                        @endif
+                    </li>
+                </ul>
+                <button class="c-btn_large" type="submit">メールを送信</button>
             </form>
         </div>
     </div>
