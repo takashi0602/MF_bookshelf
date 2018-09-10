cb4db2086f305ab42c385b92bbf28ee204e1ce30
takashi <takashioicsn@gmail.com>
2018-09-11 00:00:19 +0900

    header bootstrap化

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,20 +1,27 @@
 @section('header')
-    <header class="l-header">
-        <img src="{{ asset('img/logo.png') }}" alt="" class="l-headerTitle_img">
-        <h1 class="l-headerTitle_header"><a href="{{ url('/') }}" class="l-headerTitle_link">{{ config('app.name', 'Laravel') }}</a></h1>
-        <nav class="l-headerNav">
-            <ul class="l-headerMenu">
-                @guest
-                    <li class="l-headerMenu_list"><a href="{{ route('login') }}" class="l-headerMenu_link"><i class="fa fa-sign-in" aria-hidden="true"></i> サインイン</a></li>
-                    <li class="l-headerMenu_list"><a href="{{ route('register') }}" class="l-headerMenu_link"><i class="fa fa-user-plus" aria-hidden="true"></i> サインアップ</a></li>
-                @else
-                    <li class="l-headerMenu_list"><a href="{{ url('/private') }}" class="l-headerMenu_link"><i class="fa fa-user" aria-hidden="true"></i> じぶんの本棚</a></li>
-                    <li class="l-headerMenu_list"><a href="{{ url('/public') }}" class="l-headerMenu_link"><i class="fa fa-users" aria-hidden="true"></i> みんなの本棚</a></li>
-                    <li class="l-headerMenu_list"><a href="{{ url('/mypage') }}" class="l-headerMenu_link">{{ Auth::user()->name }}</a></li>
-                    <li><a href="{{ route('logout') }}" class="l-headerMenu_link" onclick="event.preventDefault(); document.getElementById('headerMenu_form').submit();"><i class="fa fa-sign-out" aria-hidden="true"></i> サインアウト</a></li>
-                    <form id="headerMenu_form" class="l-headerMenu_form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
-                @endguest
-            </ul>
-        </nav>
+    <header class="header">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a href="{{ url('/') }}" class="navbar-brand">
+          <img src="{{ asset('img/logo.png') }}" style="width: 100px;">
+          <h3 class="d-inline-block text-primary">{{ config('app.name', 'Laravel') }}</h3>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul class="navbar-nav">
+            @guest
+            <li class="nav-item"><a href="{{ route('login') }}" class="nav-link text-primary"><i class="fa fa-sign-in" aria-hidden="true"></i> サインイン</a></li>
+            <li class="nav-item"><a href="{{ route('register') }}" class="nav-link text-primary"><i class="fa fa-user-plus" aria-hidden="true"></i> サインアップ</a></li>
+            @else
+              <li class="nav-item"><a href="{{ url('/private') }}" class="nav-link text-primary"><i class="fa fa-user" aria-hidden="true"></i> じぶんの本棚</a></li>
+              <li class="nav-item"><a href="{{ url('/public') }}" class="nav-link text-primary"><i class="fa fa-users" aria-hidden="true"></i> みんなの本棚</a></li>
+              <li class="nav-item"><a href="{{ url('/mypage') }}" class="nav-link text-primary">{{ Auth::user()->name }}</a></li>
+              <li><a href="{{ route('logout') }}" class="nav-link text-primary" onclick="event.preventDefault(); document.getElementById('headerMenu_form').submit();"><i class="fa fa-sign-out" aria-hidden="true"></i> サインアウト</a></li>
+              <form id="headerMenu_form" class="nav-link text-primary" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
+            @endguest
+          </ul>
+        </div>
+      </nav>
     </header>
 @endsection
