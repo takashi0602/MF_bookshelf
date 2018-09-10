5ba79aaa52da57c9852f11dafd160f761bca007a
Kazuki Tobita <kazukiti201@gmail.com>
2018-09-11 00:54:05 +0900

    Modify Copyright

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 @section('footer')
     <footer class="footer bg-primary d-flex align-items-center justify-content-center" style="height: 100px;">
-        <div class="text-white">Copyright 2017 ASS All Rights Reserved</div>
+        <div class="text-white">Copyright 2017 - 2018 ASS All Rights Reserved</div>
     </footer>
 @endsection
